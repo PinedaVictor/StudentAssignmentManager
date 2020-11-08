23e212cb0e6c000d739fb6bffc7b3428122fa32a
jdavalos1 <jcharlied13@gmail.com>
2020-11-07 23:02:28 -0800

    Got a bare minimum horizontal scroll bar going
    Using a js module (react-horizontal-scrolling-menu) a scollable nav bar was created.

--- src/Components/ReusableParts/CustomNavBar.tsx
+++ src/Components/ReusableParts/CustomNavBar.tsx
@@ -1,11 +1,37 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { Component } from 'react';
+import ScrollMenu from 'react-horizontal-scrolling-menu';
+import {NavBarStyle} from './CustomNavBar.style';
 
+const NavBarItems = (list: string[], selected: any) => 
+    list.map(element => {
+        const name = element;
 
-export const CustomNavBar: React.FC = () => {
-    return (
-        <Container>
+        return (
+            <div key={name} className={`menu-item ${selected ? 'active' : ''}`}>
+                {element}
+            </div>);
+    });
 
-        </Container>
-    )
-}
+type Props = {
+    list: string[];
+    selected: string;
+    onSelect?: any;
+};
+
+const CustomNavBar: React.FC<Props> = ({list, selected, onSelect}) => (
+    // TODO: need to add styles
+    <NavBarStyle>
+        <ScrollMenu 
+            data={NavBarItems(list, selected)}
+            arrowLeft={<div className='arrow-prev'>&lt;</div>}
+            arrowRight={<div className='arrow-next'>&gt;</div>}
+            selected={selected}
+            onSelect={onSelect}
+            hideSingleArrow={true}
+            hideArrows={true}
+            alignOnResize={true}
+        />
+    </NavBarStyle>
+)
+
+export default CustomNavBar;
--- src/Components/student-tools/Exams.tsx
+++ src/Components/student-tools/Exams.tsx
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { useState } from 'react';
 import { createStyles, makeStyles, TextField, Theme } from "@material-ui/core";
 import { create } from "ts-style";
+import CustomNavBar from '../ReusableParts/CustomNavBar';
+
+const list = [
+    'item1',
+    'item2',
+    'item3',
+]
+
 
 export const Exams: React.FC = () => {
+    const selected = 'item1';
 
     return (
-        <div style = {styles.pageLayout}>
-            
+        <div className="Exams">
+            <CustomNavBar list={list} selected={selected}/>
         </div>
     )
 }
 
-const styles = create({
-    pageLayout: {
-        display: 'grid',
-        paddingTop: 50,
-        justifyContent: "center",
-        alignItems: 'center'
-    },
-})
