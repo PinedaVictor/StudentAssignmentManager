9dbcc617b2571d3723d7f08e33e81b18dd35a846
VartanKaze93 <vartkaze@gmail.com>
2020-11-09 18:58:01 -0800

    finished course page modal for editing/adding a course.

--- src/Components/student-tools/Courses.tsx
+++ src/Components/student-tools/Courses.tsx
@@ -2,13 +2,14 @@
 Look up either React lodash, or React debounce for potential performance improvements with materialui update components.
 */
 
-import React, { useContext, useState } from "react";
-import {DynamicCard}  from "../ReusableParts/DynamicCard";
+import React, { useState } from "react";
+import { DynamicCard }  from "../ReusableParts/DynamicCard";
 import { create } from "ts-style";
-import { createStyles, makeStyles, Slider, TextField, Theme, Typography } from "@material-ui/core";
+import { createStyles, makeStyles, Slider, TextField, Theme } from "@material-ui/core";
 import { CustomButton } from "../ReusableParts/CustomButton";
 import { CustomModal } from "../ReusableParts/CustomModal";
 
+import { app } from "../../Database/initFirebase"
 
 const courses = [
   {
@@ -30,6 +31,50 @@ const courses = [
       project: 30,
       exam: 40,
       quiz: 10
+    }
+  },
+
+  {
+    id: "buq5gb",
+    name: "Phil 101",
+    email: "dude.broson@gmail.com",
+    officeHours: "Mo 7:00pm - 9:45pm",
+    lateWorkPolicy: "Turn it in whenever, I don't care.",
+
+    gradingScale: {
+      AMinus: 90,
+      BMinus: 80,
+      CMinus: 70,
+      DMinus: 60
+    },
+
+    gradeWeights: {
+      homework: 25,
+      project: 0,
+      exam: 50,
+      quiz: 25
+    }
+  },
+
+  {
+    id: "ha4n7",
+    name: "BS 101",
+    email: "blah.mehson@gmail.com",
+    officeHours: "Tu/Th 12:00pm - 2:00pm",
+    lateWorkPolicy: "No late work allowed",
+
+    gradingScale: {
+      AMinus: 95,
+      BMinus: 90,
+      CMinus: 85,
+      DMinus: 80
+    },
+
+    gradeWeights: {
+      homework: 0,
+      project: 0,
+      exam: 100,
+      quiz: 0
     }
   }
 ]
@@ -72,48 +117,76 @@ interface ModalFields {
   email: string,
   hours: string,
   latePolicy: string,
-  gradeScale: string,
-  hwWeight: number,
-  projWeight: number,
-  examWeight: number,
-  quizWeight: number
+  gradeScale: {
+    AMinus : number,
+    BMinus : number,
+    CMinus : number,
+    DMinus : number
+  },
+
+  gradeWeights: {
+    homework : number,
+    project : number,
+    exam : number,
+    quiz : number
+  }
 }
 
 export const Courses: React.FC = () => {
   const classes = useStyles()
 
   const [cardModal, setCardModal] = useState(false);
-  const [modalStage, setModalStage] = useState("closed")
+  const [modalStage, setModalStage] = useState(0)
 
   const [EditCourse, setEditCourse] = useState<ModalFields>({
     courseName: "",
     email: "",
     hours: "",
     latePolicy: "",
-    gradeScale: "",
-    hwWeight: 0,
-    projWeight: 0,
-    examWeight: 0,
-    quizWeight: 0
+    gradeScale: {
+      AMinus: 0,
+      BMinus: 0,
+      CMinus: 0,
+      DMinus: 0,
+    },
+
+    gradeWeights: {
+      homework: 0,
+      project: 0,
+      exam: 0,
+      quiz: 0
+    }
   })
 
-  const cycleModalStage = () => {
-    if (modalStage === "closed"){
+  const cycleModalStage = (direction: 1 | -1) => {
+
+    if (modalStage === 0){
       setCardModal(true)
-      setModalStage("general")
     }
 
-    else if (modalStage === "general")
-      setModalStage("grading scale")
-
-    else if (modalStage === "grading scale")
-      setModalStage("grade weights")
-
-    else {
-      setCardModal(false)
-      setModalStage("closed")
-    }
     
+    setModalStage(modalStage + direction)
+
+  }
+
+  const submitModal = () => {
+          
+    setModalStage(0)
+    setCardModal(false)
+    clearTextInputs()
+  }
+
+  const getModalForm = () => {
+    if (modalStage === 1)
+      return CourseModal_Stage1
+
+    else if (modalStage === 2)
+      return CourseModal_Stage2
+
+    else if (modalStage === 3)
+      return CourseModal_Stage3
+
+    else return (<form></form>)
   }
 
   const clearTextInputs = () => {
@@ -121,55 +194,72 @@ export const Courses: React.FC = () => {
       courseName: "",
       email: "",
       latePolicy: "",
-      gradeScale: "",
       hours: "",
-      hwWeight: 0,
-      projWeight: 0,
-      examWeight: 0,
-      quizWeight: 0,
+
+      gradeScale: {
+        AMinus: 0,
+        BMinus: 0,
+        CMinus: 0,
+        DMinus: 0,
+      },
+  
+      gradeWeights: {
+        homework: 0,
+        project: 0,
+        exam: 0,
+        quiz: 0
+      }
     })
-  }
-
-  const submitModal = () => {
-
-
-    
-    setModalStage("closed")
-    setCardModal(false)
-    clearTextInputs()
   }
 
   const deleteCourse = () => {
 
   }
 
-  const closeModal = () => {
-
-    clearTextInputs()
-    setCardModal(false)
-  }
-
-  const setModalInput = (field: "courseName" | "email" | "latePolicy" | "gradeScale" | "hwWeight" | "examWeight" | "projWeight" | "hours" | "quizWeight", value: string) => {
+  const setModalInput = (field: "courseName" | "email" | "latePolicy"  | "hours" | "A-" | "B-" | "C-" | "D-" | "hwWeight" | "examWeight" | "projWeight" | "quizWeight", value: string) => {
    setEditCourse( {
       courseName: (field === "courseName")  ?  value : EditCourse.courseName,
       email: (field === "email")            ?            value : EditCourse.email,
       latePolicy: (field === "latePolicy")  ?   value : EditCourse.latePolicy,
-      gradeScale: (field === "gradeScale")  ?   value : EditCourse.gradeScale,
       hours: (field === "hours")            ?   value : EditCourse.hours,
-      hwWeight: (field === "hwWeight")      ?   validateWeight(value) : EditCourse.hwWeight,
-      projWeight: (field === "projWeight")  ?   validateWeight(value) : EditCourse.projWeight,
-      examWeight: (field === "examWeight")  ?   validateWeight(value) : EditCourse.examWeight,
-      quizWeight: (field === "quizWeight")  ?   validateWeight(value) : EditCourse.quizWeight
+
+      gradeScale: (field === "A-" || field === "B-" || field === "C-" || field === "D-")          
+        ? {
+          AMinus: (field === "A-") ? validateNumInput(value) : EditCourse.gradeScale.AMinus,
+          BMinus: (field === "B-") ? validateNumInput(value) : EditCourse.gradeScale.BMinus,
+          CMinus: (field === "C-") ? validateNumInput(value) : EditCourse.gradeScale.CMinus,
+          DMinus: (field === "D-") ? validateNumInput(value) : EditCourse.gradeScale.DMinus
+          } 
+        : EditCourse.gradeScale,
+
+      gradeWeights: (field === "hwWeight" || field === "examWeight" || field === "projWeight" || field === "quizWeight")
+        ? {
+          homework: (field === "hwWeight") ? validateNumInput(value) : EditCourse.gradeWeights.homework,
+          project: (field === "projWeight") ? validateNumInput(value) : EditCourse.gradeWeights.project,
+          exam: (field === "examWeight") ? validateNumInput(value) : EditCourse.gradeWeights.exam,
+          quiz: (field === "quizWeight") ? validateNumInput(value) : EditCourse.gradeWeights.quiz,
+          }
+        : EditCourse.gradeWeights
+
     })
   }
 
-  const validateWeight = (value: string) => {
+  const validateNumInput = (value: string) => {
     var temp = parseFloat(value)
     
     if(temp === null || temp === undefined || Number.isNaN(temp))
       return 0
 
     else return temp 
+  }
+
+  const validateScaleValues = (field1: number, field2: number, field3: number) => {
+    var difference = 100 - field1 - field2 - field3;
+    
+    if (difference >= 0)
+      return difference;
+
+    else return 0;
   }
 
   const CourseModal_Stage1 = (
@@ -245,40 +335,90 @@ export const Courses: React.FC = () => {
 
           <CustomButton
           dimensions = {{ width: 70 , height: 40 , margin: 20 }}
-          onClick = {() => cycleModalStage()}
+          onClick = {() => cycleModalStage(1)}
           theme = "edit"
           title = "Next"
           />
 
           <CustomButton
           dimensions = {{ width: 70 , height: 40 , margin: 20 }}
-          onClick = {() => closeModal()}
+          onClick = {() => cycleModalStage(-1)}
           theme = "delete"
-          title = "Back"
+          title = "Close"
           />
         </div>
 
     </form>
     
-)
+  )
 
 const CourseModal_Stage2 = (
   <form  className={classes.root} noValidate autoComplete="off">
 
     <div style = {styles.modalHeader}> Grading Scale </div>
-    <div>
-      <TextField 
-      value = {EditCourse.gradeScale}
-      onChange = {(e) => {setModalInput("gradeScale", e.target.value)}}
-      label = {"Grading Scale"}
-      placeholder = "A- = 85%, B- = ..."
-      variant = "filled"
-      required = {true}
-      InputProps = {{ className: classes.textNormal}}
-      InputLabelProps = {{ className: classes.textNormal}}
-      className = {classes.textField}
-      size = "small"
-      />
+    
+    <div style = {styles.weightSection}>
+      <div style = {styles.labelStyle} >A-</div>
+
+      <div style = {styles.weightRow}>
+        <Slider
+        name = "A-"
+        min = {0}
+        max = {validateScaleValues(EditCourse.gradeScale.BMinus, EditCourse.gradeScale.CMinus, EditCourse.gradeScale.DMinus)}
+        value = {EditCourse.gradeScale.AMinus}
+        onChange = {(event, value) => setModalInput("A-", value.toString())}
+        />
+
+        <text style = {{fontSize: 18}}> {EditCourse.gradeScale.AMinus}%</text>
+      </div>
+    </div>
+
+    <div style = {styles.weightSection}>
+      <div style = {styles.labelStyle} >B-</div>
+
+      <div style = {styles.weightRow}>
+        <Slider
+        name = "B-"
+        min = {0}
+        max = {validateScaleValues(EditCourse.gradeScale.AMinus, EditCourse.gradeScale.CMinus, EditCourse.gradeScale.DMinus)}
+        value = {EditCourse.gradeScale.BMinus}
+        onChange = {(event, value) => setModalInput("B-", value.toString())}
+        />
+
+        <text style = {{fontSize: 18}}> {EditCourse.gradeScale.BMinus}%</text>
+      </div>
+    </div>
+
+    <div style = {styles.weightSection}>
+      <div style = {styles.labelStyle} >C-</div>
+
+      <div style = {styles.weightRow}>
+        <Slider
+        name = "C-"
+        min = {0}
+        max = {validateScaleValues(EditCourse.gradeScale.AMinus, EditCourse.gradeScale.BMinus, EditCourse.gradeScale.DMinus)}
+        value = {EditCourse.gradeScale.CMinus}
+        onChange = {(event, value) => setModalInput("C-", value.toString())}
+        />
+
+        <text style = {{fontSize: 18}}> {EditCourse.gradeScale.CMinus}%</text>
+      </div>
+    </div>
+
+    <div style = {styles.weightSection}>
+      <div style = {styles.labelStyle} >D-</div>
+
+      <div style = {styles.weightRow}>
+        <Slider
+        name = "D-"
+        min = {0}
+        max = {validateScaleValues(EditCourse.gradeScale.AMinus, EditCourse.gradeScale.BMinus, EditCourse.gradeScale.CMinus)}
+        value = {EditCourse.gradeScale.DMinus}
+        onChange = {(event, value) => setModalInput("D-", value.toString())}
+        />
+
+        <text style = {{fontSize: 18}}> {EditCourse.gradeScale.DMinus}%</text>
+      </div>
     </div>
 
     <div style = {{
@@ -292,46 +432,88 @@ const CourseModal_Stage2 = (
 
         <CustomButton
         dimensions = {{ width: 70 , height: 40 , margin: 20 }}
-        onClick = {() => cycleModalStage()}
+        onClick = {() => cycleModalStage(1)}
         theme = "edit"
         title = "Next"
         />
 
         <CustomButton
         dimensions = {{ width: 70 , height: 40 , margin: 20 }}
-        onClick = {() => closeModal()}
+        onClick = {() => cycleModalStage(-1)}
         theme = "delete"
         title = "Back"
         />
       </div>
   </form>
-)
+  )
 
 const CourseModal_Stage3 = (
   <form  className={classes.root} noValidate autoComplete="off">
 
     <div style = {styles.modalHeader}> Grade Weights </div>
+
     <div style = {styles.weightSection}>
-      <div style = {styles.labelStyle} >Homework Percentage</div>
+      <div style = {styles.labelStyle} >Homework</div>
 
       <div style = {styles.weightRow}>
         <Slider
-        name = "hwSlider"
+        name = "homework"
         min = {0}
-        max = {100}
-        value = {EditCourse.hwWeight}
+        max = {validateScaleValues(EditCourse.gradeWeights.project, EditCourse.gradeWeights.exam, EditCourse.gradeWeights.quiz)}
+        value = {EditCourse.gradeWeights.homework}
         onChange = {(event, value) => setModalInput("hwWeight", value.toString())}
         />
 
-        <input
-        style = {styles.inputStyle}
-        type = "text"
-        value = {EditCourse.hwWeight}
-        onChange = {(e) => setModalInput("hwWeight", e.target.value)}
-        />
-        <text style = {styles.inputPost}>%</text>
+        <text style = {{fontSize: 18}}> {EditCourse.gradeWeights.homework}%</text>
       </div>
+    </div>
 
+    <div style = {styles.weightSection}>
+      <div style = {styles.labelStyle} >Projects</div>
+
+      <div style = {styles.weightRow}>
+        <Slider
+        name = "project"
+        min = {0}
+        max = {validateScaleValues(EditCourse.gradeWeights.homework, EditCourse.gradeWeights.exam, EditCourse.gradeWeights.quiz)}
+        value = {EditCourse.gradeWeights.project}
+        onChange = {(event, value) => setModalInput("projWeight", value.toString())}
+        />
+
+        <text style = {{fontSize: 18}}> {EditCourse.gradeWeights.project}%</text>
+      </div>
+    </div>
+
+    <div style = {styles.weightSection}>
+      <div style = {styles.labelStyle} >Exams</div>
+
+      <div style = {styles.weightRow}>
+        <Slider
+        name = "exam"
+        min = {0}
+        max = {validateScaleValues(EditCourse.gradeWeights.homework, EditCourse.gradeWeights.project, EditCourse.gradeWeights.quiz)}
+        value = {EditCourse.gradeWeights.exam}
+        onChange = {(event, value) => setModalInput("examWeight", value.toString())}
+        />
+
+        <text style = {{fontSize: 18}}> {EditCourse.gradeWeights.exam}%</text>
+      </div>
+    </div>
+
+    <div style = {styles.weightSection}>
+      <div style = {styles.labelStyle} >Quizzes</div>
+
+      <div style = {styles.weightRow}>
+        <Slider
+        name = "quiz"
+        min = {0}
+        max = {validateScaleValues(EditCourse.gradeWeights.homework, EditCourse.gradeWeights.project, EditCourse.gradeWeights.exam)}
+        value = {EditCourse.gradeWeights.quiz}
+        onChange = {(event, value) => setModalInput("quizWeight", value.toString())}
+        />
+
+        <text style = {{fontSize: 18}}> {EditCourse.gradeWeights.quiz}%</text>
+      </div>
     </div>
 
     <div style = {{
@@ -352,13 +534,13 @@ const CourseModal_Stage3 = (
 
         <CustomButton
         dimensions = {{ width: 70 , height: 40 , margin: 20 }}
-        onClick = {() => closeModal()}
+        onClick = {() => cycleModalStage(-1)}
         theme = "delete"
         title = "Back"
         />
       </div>
   </form>
-)
+  )
 
   return (
     <div style = {styles.pageLayout}>
@@ -372,7 +554,7 @@ const CourseModal_Stage3 = (
       }}>
         <CustomButton
         title = "Add Class"
-        onClick = {() => cycleModalStage()}
+        onClick = {() => cycleModalStage(1)}
         theme = "default"
         dimensions = {{
           width: "auto",
@@ -394,19 +576,17 @@ const CourseModal_Stage3 = (
           }}
           width={"auto"}
           type="standard"
-          editClick = {() => cycleModalStage()}
+          editClick = {() => cycleModalStage(1)}
           deleteClick = {() => deleteCourse()}
           /> 
         ))}
       </div>
 
-      <CustomModal
-      layout = {(modalStage === "general") ? CourseModal_Stage1 : 
-                (modalStage === "grading scale") ? CourseModal_Stage2 : 
-                (modalStage === "grade weights") ? CourseModal_Stage3 :
-                <form></form>}
-      modalState = {cardModal}
-      />
+      {(modalStage !== 0) &&
+        <CustomModal
+          layout = {getModalForm()}
+          modalState = {cardModal}
+        />}
     </div>
   );
 };
@@ -447,7 +627,7 @@ const styles = create({
 
   weightRow: {
     display: 'grid',
-    gridTemplateColumns: "85% 10% 5%",
+    gridTemplateColumns: "350px auto",
     columnGap: "1rem"
   },
 
@@ -461,9 +641,5 @@ const styles = create({
   inputStyle: {
     fontSize: 14,
     width: 45
-  },
-
-  inputPost: {
-    fontSize: 18
   }
 })
